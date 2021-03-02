77cc0a21d5935323bbd2d7e90b8bcef8a8365c7d
0culty <tazzang0921@gmail.com>
2021-03-02 22:19:49 +0900

    ~~

--- 파이썬_알고리즘_인터뷰/6. 문자열 조작/4-1.py
+++ 파이썬_알고리즘_인터뷰/6. 문자열 조작/4-1.py
@@ -12,3 +12,17 @@ class Solution:
         counts = collections.Counter(words)
         # 가장 흔하게 등장하는 단어의 첫 번째 인덱스 리턴
         return counts.most_common(1)[0][0]
+
+# 내 풀이
+# from collections import defaultdict
+# import re
+
+# class Solution:
+#     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+#         words = re.sub(r'[^\w]', ' ', paragraph).lower().split()
+#         dic = defaultdict(int)
+#         for w in words:
+#             if w not in banned:
+#                 dic[w] += 1
+
+#         return max(dic.keys(), key=(lambda k: dic[k]))
